7efd96e21658588c2838006ba1607f6a73221ccd
0xJilan <107534852+0xJilan@users.noreply.github.com>
2022-10-20 16:36:47 +0200

    add all test for ERC kind of contract

--- Test/TestERC20/test/test.js
+++ Test/TestERC20/test/test.js
@@ -7,7 +7,7 @@ contract("MyToken", (accounts) => {
   const _symbol = "ALY";
   const _initialSupply = new BN(10000);
   const _owner = accounts[0];
-  const _recipientAsdress = accounts[1];
+  const _recipient = accounts[1];
   const _decimal = new BN(18);
 
   let myTokenInstance;
@@ -18,5 +18,30 @@ contract("MyToken", (accounts) => {
 
   it("has a name", async () => {
     expect(await myTokenInstance.name()).to.equal(_name);
+  });
+  it("has a symbol", async () => {
+    expect(await myTokenInstance.symbol()).to.equal(_symbol);
+  });
+  it("has a decimal", async () => {
+    expect(await myTokenInstance.decimals()).to.be.bignumber.equal(_decimal);
+  });
+  it("check first balance", async () => {
+    expect(await myTokenInstance.balanceOf(_owner)).to.be.bignumber.equal(
+      _initialSupply
+    );
+  });
+  it("check balance after transfer", async () => {
+    expect(await myTokenInstance.balanceOf(_recipient)).to.be.bignumber.equal(
+      new BN(0)
+    );
+
+    await myTokenInstance.transfer(_recipient, new BN(100), { from: _owner });
+
+    expect(await myTokenInstance.balanceOf(_owner)).to.be.bignumber.equal(
+      new BN(9900)
+    );
+    expect(await myTokenInstance.balanceOf(_recipient)).to.be.bignumber.equal(
+      new BN(100)
+    );
   });
 });
