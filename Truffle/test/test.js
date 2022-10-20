a3fc24c3befe563c36242e7cbd7e79ef85f310ab
0xJilan <107534852+0xJilan@users.noreply.github.com>
2022-10-20 15:48:18 +0200

    add chai and openzeppelin

--- Truffle/test/test.js
+++ Truffle/test/test.js
@@ -1,12 +1,16 @@
+const { BN, expectRevert, expectEvent } = require("@openzeppelin/test-helpers");
+const { expect } = require("chai");
+const constants = require("@openzeppelin/test-helpers/src/constants");
+
 const SimpleStorage = artifacts.require("SimpleStorage");
 
 contract("SimpleStorage", (accounts) => {
+  let owner = accounts[0];
+
   it("...should store the value 89", async () => {
     const simpleStorageInstance = await SimpleStorage.deployed();
-
-    await simpleStorageInstance.set(89, { from: accounts[0] });
+    await simpleStorageInstance.set(89, { from: owner });
     const storedData = await simpleStorageInstance.get.call();
-
     assert.equal(storedData, 89);
   });
 });
