69dad0a98a4f916d4855f19c9adbc06073c74aab
0xJilan <107534852+0xJilan@users.noreply.github.com>
2022-10-26 19:14:21 +0200

    start to refacto to host more than one contract

---
+++ Test/TestERC20/contracts/SimpleStorage.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.17;
+
+contract SimpleStorage {
+    enum StorageData {
+        VALUE1,
+        VALUE2,
+        VALUE3
+    }
+    StorageData public storageData;
+
+    event dataStored(StorageData _data, address _addr);
+
+    function get() public view returns (StorageData) {
+        return storageData;
+    }
+
+    function set(StorageData _storageData) public {
+        require(
+            _storageData != StorageData.VALUE1,
+            "vous ne pouvez pas mettre la premiere valeur!"
+        );
+        storageData = _storageData;
+        emit dataStored(_storageData, msg.sender);
+    }
+}
---
+++ Test/TestERC20/test/SimpleStorage.js
@@ -0,0 +1,35 @@
+const SimpleStorage = artifacts.require("SimpleStorage");
+const { BN, expectRevert, expectEvent } = require("@openzeppelin/test-helpers");
+const { expect } = require("chai");
+
+contract("SimpleStorage", (accounts) => {
+  const owner = accounts[0];
+  let simpleStorageInstance;
+
+  describe("test complet", function () {
+    beforeEach(async function () {
+      simpleStorageInstance = await SimpleStorage.new({ from: owner });
+    });
+
+    it("...should store the value 2.", async () => {
+      await simpleStorageInstance.set(2, { from: owner });
+      const storedData = await simpleStorageInstance.storageData.call();
+      expect(storedData).to.be.bignumber.equal(new BN(2));
+    });
+
+    it("should revert on value 0", async () => {
+      await expectRevert(
+        simpleStorageInstance.set(new BN(0), { from: owner }),
+        "vous ne pouvez pas mettre la premiere valeur"
+      );
+    });
+
+    it("should emit event on set", async () => {
+      expectEvent(
+        await simpleStorageInstance.set(new BN(2), { from: accounts[0] }),
+        "dataStored",
+        { _data: new BN(2), _addr: owner }
+      );
+    });
+  });
+});
