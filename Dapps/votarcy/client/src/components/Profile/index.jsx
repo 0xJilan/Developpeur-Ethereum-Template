35e091fc3ab92e78b937d879e967086523c5868a
0xJilan <107534852+0xJilan@users.noreply.github.com>
2022-11-22 20:00:14 +0100

    fix display address

--- Dapps/votarcy/client/src/components/Profile/index.jsx
+++ Dapps/votarcy/client/src/components/Profile/index.jsx
@@ -4,8 +4,12 @@ const Profile = ({ props }) => {
   const { accounts, isAdmin } = props;
   return accounts && accounts[0] ? (
     <div className="Connected">
-      <Jazzicon diameter={64} seed={jsNumberForAddress(accounts[0])} />
-      <p>{accounts[0].slice(0, 5) + "..." + accounts[0].slice(-4)}</p>
+      <Jazzicon
+        style={{ zIndex: 1000 }}
+        diameter={64}
+        seed={jsNumberForAddress(accounts[0])}
+      />
+      <h2>{accounts[0].slice(0, 5) + "..." + accounts[0].slice(-4)}</h2>
       <p>{isAdmin ? "Admin" : "Voter "} </p>
     </div>
   ) : (
